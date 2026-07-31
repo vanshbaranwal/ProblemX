58492802da9ef9b39713813c70eb49b9645e43fb
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-08-01 03:15:39 +0530

    set up PostgreSQL and prisma with initial migration

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const app = express();
 
 
+
 app.listen(process.env.PORT, () => {
-    console.log("server is running on the port 8080");
-})
+    console.log(`the server is running on the port: ${process.env.PORT}`);
+});
 
