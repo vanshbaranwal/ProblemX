29f33d0fce186866224bbce760d0c771b2332389
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-09-22 23:11:58 +0530

    feat(frontend): add initial page routing and protected navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import { Routes, Route, Navigate } from "react-router-dom"
-import HomePage from './page/HomePage'
-import LoginPage from './page/LoginPage'
-import SignUpPage from './page/SignUpPage'
-
+import HomePage from './page/homePage'
+import LoginPage from './page/loginPage'
+import SignUpPage from './page/signupPage'
 
 const App = () => {
+  
+  let authUser = null
+  
   return (
+
     <div className='flex flex-col items-center justify-start'>
       <Routes>
-
+        
         <Route
-        path='/'
-        element={<HomePage/>}
+        path="/"
+        element={authUser ? <HomePage/> : <Navigate to={"/login"}/> }
+        />
+        
+        <Route
+        path="/login"
+        element={!authUser ? <LoginPage/> : <Navigate to={"/"}/> }
         />
 
         <Route
-        path='/login'
-        element={<LoginPage/>}
+        path="/signup"
+        element={!authUser ? <SignUpPage/> : <Navigate to={"/"}/> }
         />
 
-        <Route
-        path='/signup'
-        element={<SignUpPage/>}
-        />
       </Routes>
     </div>
   )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
