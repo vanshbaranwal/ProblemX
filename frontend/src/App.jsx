7181b4dbb8cbaf252e263d5f44aa1cfd6dfe8ee0
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-09-25 22:28:34 +0530

    feat(auth): connect login and signup forms to auth API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
 import HomePage from './page/homePage'
 import LoginPage from './page/loginPage'
 import SignUpPage from './page/signupPage'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
 
 const App = () => {
   
-  let authUser = null
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  if(isCheckingAuth && !authUser){
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin'/>
+      </div>
+    )
+  }
   
   return (
 
     <div className='flex flex-col items-center justify-start'>
+      <Toaster/>
       <Routes>
         
         <Route
